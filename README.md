1001f7aa0c0b408e9244c2442562ab696309c181
Anh Tu <anhtu8865@gmail.com>
2025-04-21 15:38:52 +0700

    feat: init source

---
+++ README.md
@@ -0,0 +1,13 @@
+## Install python 3.12.8
+
+## Install dependencies
+
+```bash
+pip install -r requirements.txt
+```
+
+## Run the app
+
+```bash
+uvicorn main:app --reload
+```
---
+++ database.py
@@ -0,0 +1,13 @@
+from dotenv import load_dotenv
+from motor.motor_asyncio import AsyncIOMotorClient
+
+import os
+
+load_dotenv()
+# MongoDB connection details
+MONGODB_URI = os.getenv("MONGODB_URI")
+DATABASE_NAME = os.getenv("DATABASE_NAME")  # Replace with your database name
+
+# Initialize MongoDB client
+client = AsyncIOMotorClient(MONGODB_URI)
+db = client[DATABASE_NAME]
